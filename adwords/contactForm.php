de4527ccddb5a940f143f35239d7f4350b0df929
ancao2504 <pullnolovesmile@gmail.com>
2019-08-09 08:47:31 +0700

    Project BDSNP

--- adwords/contactForm.php
+++ adwords/contactForm.php
@@ -1,89 +1,3 @@
-<!-- <script type="text/javascript">
-    sendContactInfoBDS();
-    function sendContactInfoBDS() {
-        $(document).ready(function() {
-            $('body').on('click','.start-submit-contact', function() {
-                var valueGmail = document.getElementById("contentGmail").value;
-                var valuePhone = document.getElementById("contentPhone").value;
-                var valueMessage = document.getElementById("contentSendChat").value;
-                var errorMessage = document.getElementById("error-message");
-
-                errorMessage.style.padding = "10px";
-                var text;
-                if (valueGmail == '') {
-                    text = "Vui lòng nhập email";
-                    errorMessage.innerHTML = text;
-                    return false;
-                }
-                var regexEmailCheck = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i;
-                if (regexEmailCheck.test(valueGmail) == false){
-                    text = "Email chưa đúng định dạng";
-                    errorMessage.innerHTML = text;
-                    return false;  
-                }
-                if(valueGmail.length < 6){
-                    text = "Email ít nhất là 6 kí tự";
-                    errorMessage.innerHTML = text;
-                    return false;
-                }
-        
-                // CHECK VALIDATE MOBILE
-                var regexCheckPhone = /((09|03|07|08|05)+([0-9]{8})\b)/g;
-                if(valuePhone == '') {
-                    text = "Vui lòng nhập số điện thoại";
-                    errorMessage.innerHTML = text;
-                    return false;
-                }
-                if (isNaN(valuePhone) || valuePhone.length != 10) {
-                    text = "Vui lòng kiểm tra lại số điện thoại";
-                    errorMessage.innerHTML = text;
-                    return false;
-                }
-                if (regexCheckPhone.test(valuePhone) == false) {
-                    text = "Số điện thoại của bạn không đúng định dạng";
-                    errorMessage.innerHTML = text;
-                    return false;
-                }
-                // CHECK CONTENT SEND
-                if(valueMessage == ''){
-                    text = "Vui lòng nhập nội dung gửi";
-                    errorMessage.innerHTML = text;
-                    return false;
-                }
-                if(valueMessage.length < 60){
-                    text = "Nội dung ít nhất là 60 kí tự";
-                    errorMessage.innerHTML = text;
-                    return false;
-                }
-                var chatID = '-325119794';
-                // var chatID = '@nhom_test_nhe';
-                var keyAPI = '936786321:AAGe3QiE-QV-p1pLK44wlVAbAfkO-_1lprw';
-                var urlTelegramAPI = 'https://api.telegram.org/bot'+keyAPI+'/sendMessage?chat_id='+chatID+'&text='+valueGmail+" - "+valuePhone+" - "+valueMessage+'';
-                var successSend = document.getElementById('success-message');
-                successSend.style.padding = "10px";
-                $.ajax({
-                    url: urlTelegramAPI,
-                    type: 'POST',
-                    data: { valueGmail : valueGmail,
-                            valuePhone : valuePhone,
-                            valueMessage : valueMessage
-                    }, success: function () {
-                        $('#formSendInfoContact input[id="contentGmail"]').val('');
-                        $('#formSendInfoContact input[id="contentPhone"]').val('');
-                        $('#formSendInfoContact textarea[id="contentSendChat"]').val('');
-                        $.ajaxSetup({cache:false});
-                    }
-                });
-                text = "Nội dung đã gửi thành công";
-                successSend.innerHTML = text;
-                $('#error-message').hide();
-                setTimeout(function() {$('#success-message').fadeOut('fast');}, 200);
-                return true;
-            });
-        });
-    }
-</script> -->
-
 <div class="main-section-contact-form-chatbox">
     <div class="row border-chat">
         <div class="col-md-12 col-sm-12 col-xs-12 first-section contact-form-chatbox">
@@ -99,7 +13,6 @@
             </div>
         </div>
     </div>
-    
     <form method="post" id="formSendInfoContact" onsubmit="return false;">
         <div class="row border-chat">
             <div id="error-message"></div>
